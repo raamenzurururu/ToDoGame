e72e6230b091f870dead1cb4d386ca589f32df61
github raamenzurururu <reku9696@gmail.com>
2020-07-27 20:58:44 +0900

    rulesを追加

--- frontend/components/AddTodo.vue
+++ frontend/components/AddTodo.vue
@@ -2,16 +2,17 @@
   <v-form>
     <v-container>
       <v-row>
-        <v-col cols="12" md="4">
+        <v-col cols="12" md="5">
           <v-text-field
             v-model="title"
+            :rules="[v => !!v || 'まだまだいけるよ！']"
             :counter="15"
             label="やるべきこと"
             required
           ></v-text-field>
         </v-col>
         <v-col cols="12" md="4">
-          <v-btn @click="handleSubmit">よいしょ！</v-btn>
+          <v-btn @click="handleSubmit">決まり</v-btn>
         </v-col>
       </v-row>
     </v-container>
