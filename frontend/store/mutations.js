d3dbc4a45b5b391e9d7131f2381eb1513029e54d
github raamenzurururu <reku9696@gmail.com>
2020-08-02 11:11:27 +0900

    signUpをactionへ追加

--- frontend/store/mutations.js
+++ frontend/store/mutations.js
@@ -1,13 +1,17 @@
 const mutations = {
-  setUser(state, payload) {
-      state.currentUser = payload
-  },
-  setLoading(state, payload) {
-      state.loading = payload
-  },
-  setNotice(state, payload) {
-      state.notification = payload
-  }
+    setUser(state, payload) {
+        state.currentUser = payload
+    },
+    setLoading(state, payload) {
+        state.loading = payload
+    },
+    setNotice(state, payload) {
+        state.notification = payload
+    },
+    setError(state, payload) {
+        state.errors = payload
+        state.errors = [...state.errors, payload]
+    }
 }
 
 export default mutations
