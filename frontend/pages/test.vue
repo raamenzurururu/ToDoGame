8a6fa379d10b748ed595ce398f64cd8acbade699
github raamenzurururu <reku9696@gmail.com>
2020-08-10 20:47:42 +0900

    test

--- frontend/pages/test.vue
+++ frontend/pages/test.vue
@@ -1,5 +1,25 @@
 <template>
-  <v-hover v-slot:default="{ hover }">
-      <v-icon v-text="hover ? 'mdi-crown' : 'mdi-heart'"></v-icon>
-  </v-hover>
+  <div class="hello">
+    <p>{{ message }}</p>
+    <div>
+      <input type="text" v-model="input">
+      <button @click="doAction">Click</button>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "お名前は?",
+      input: " "
+    };
+  },
+  methods: {
+    doAction() {
+      this.message = "Hello," + this.input + "さん！";
+    }
+  }
+};
+</script>
