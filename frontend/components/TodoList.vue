74904b1badff04cb897159a085eb9f4b36c77616
github raamenzurururu <reku9696@gmail.com>
2020-08-14 12:25:12 +0900

    [add]vue-draggableのinstall

--- frontend/components/TodoList.vue
+++ frontend/components/TodoList.vue
@@ -4,49 +4,14 @@
       <h2 class="list-title">TODOS</h2>
       <v-spacer></v-spacer>
     </v-card-title>
-    <v-data-table :headers="headers" :items="todos">
-      <template v-slot:item.point="props">
-        <v-edit-dialog :return-value="props.item.point">
-          <!-- .syncを消すことでPointをリアルタイムで反映できる -->
-          {{ props.item.point }}
-          <template v-slot:input>
-            <v-select
-              @change="updatePoint(props.item.id, props.item.point)"
-              v-model="props.item.point"
-              :items="items"
-              single-line
-            ></v-select>
-          </template>
-        </v-edit-dialog>
-      </template>
-
-      <template v-slot:item.title="props">
-        <v-edit-dialog :return-value.sync="props.item.title">
-          {{ props.item.title }}
-          <template v-slot:input>
-            <v-text-field
-              @change="updateTitle(props.item.id, props.item.title)"
-              v-model="props.item.title"
-              label="Edit"
-              single-line
-              counter
-            ></v-text-field>
-          </template>
-        </v-edit-dialog>
-      </template>
-      <template v-slot:item.complete="{ item }">
-        <v-hover v-slot:default="{ hover }">
-          <v-icon
-            color="yellow"
-            @click="completeItem(item)"
-            v-text="hover ? 'mdi-crown' : 'mdi-crown-outline'"
-          ></v-icon>
-        </v-hover>
-      </template>
-      <template v-slot:item.action="{ item }">
-        <v-icon small @click="deleteItem(item)">delete</v-icon>
-      </template>
-    </v-data-table>
+    <ul>
+      <li><span>【達成ボタン】</span>【編集ボタン】</li>
+      <draggable>
+      <li v-for="todo in todos" :key="todo">
+        Point{{ todo.title }}:{{ todo.title }}
+      </li>
+      </draggable>
+    </ul>
   </v-card>
 </template>
 
@@ -55,6 +20,7 @@ const maxNumber = 11;
 const numberRange = [...Array(maxNumber).keys()];
 
 import axios from "@/plugins/axios";
+import draggable from 'vuedraggable';
 
 export default {
   props: ["todos"],
@@ -62,21 +28,7 @@ export default {
     return {
       singleSelect: true,
       selected: [],
-      search: "",
-      editOn: true,
       items: numberRange,
-      headers: [
-        {
-          text: "ToDo",
-          align: "left",
-          sortable: false,
-          value: "title"
-        },
-        { text: "Task Point(TP)", value: "point" },
-        { text: "Goal", value: "complete" },
-        // { text: "Edit", value: "edit", sortable: false },
-        { text: "Actions", value: "action", sortable: false }
-      ]
     };
   },
   computed: {
