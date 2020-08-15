97d16067d8056cb973f485d9bb3c4ac7ba83e8e2
github raamenzurururu <reku9696@gmail.com>
2020-08-15 10:07:06 +0900

    [add]Todoにsortカラムを追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_110953) do
+ActiveRecord::Schema.define(version: 2020_08_15_010039) do
 
   create_table "level_settings", force: :cascade do |t|
     t.integer "level"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_110953) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "point"
+    t.integer "sort", default: 0
   end
 
   create_table "users", force: :cascade do |t|
