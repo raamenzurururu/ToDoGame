e6b19a93b651bfadcdf12e3cb71d9515612fd279
github raamenzurururu <reku9696@gmail.com>
2020-08-19 21:38:25 +0900

    [add]indexページの変更

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -57,5 +57,18 @@
     <img class="mon" src="../assets/mon_178.gif">
     <img class="mon" src="../assets/mon_178.gif">
     <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
+    <img class="mon" src="../assets/mon_178.gif">
   </div>
 </template>
