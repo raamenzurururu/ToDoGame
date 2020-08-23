03d69de14dabffd31fbba9c66e15721646fe7285
github raamenzurururu <reku9696@gmail.com>
2020-08-23 10:30:07 +0900

    [fix]rewardページのエラー解決/保存はできない

--- backend/app/controllers/v1/rewards_controller.rb
+++ backend/app/controllers/v1/rewards_controller.rb
@@ -12,17 +12,21 @@ class V1::RewardsController < ApplicationController
     end
   end
 
+  def update
+    reward = Reward.find(params[:id])
+    reward.update(reward_params)
+      if reward.errors.present?
+        render json: { error_msg: reward.errors.full_messages }, status: :unprocessable_entity
+      else
+        render json: reward
+    end
+  end
+
   def destroy
     reward = Reward.find(params[:id])
     if reward.destroy
       render json: reward
     end
-  end
-
-  def update
-    reward = Reward.find(params[:id])
-    reward.update(reward_params)
-    render json: reward
   end
 
   def complete
