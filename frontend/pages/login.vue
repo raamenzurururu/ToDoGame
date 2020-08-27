5768f0b7f374af865064abbe6973b1701f4d5232
github raamenzurururu <reku9696@gmail.com>
2020-08-28 00:50:04 +0900

    [add]余分なコードの削除

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -206,26 +206,6 @@
         </v-card-text>
       </v-card>
     </v-dialog>
-    <!-- <form>
-      <v-text-field
-        v-model="email"
-        :counter="20"
-        label="email"
-        data-vv-name="email"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="password"
-        label="password"
-        data-vv-name="password"
-        required
-        :type="show1 ? 'text' : 'password'"
-        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-        @click:append="show1 = !show1"
-      ></v-text-field>
-      <v-btn class="mr-4" @click="login">submit</v-btn>
-      <p v-if="error" class="errors">{{ error }}</p>
-    </form> -->
   </v-container>
 </template>
 
