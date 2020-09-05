6e6799a0afdc10da9394ece287cc89091999bca9
github raamenzurururu <reku9696@gmail.com>
2020-09-06 00:04:17 +0900

    [add]loginページの言葉を改善

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -93,10 +93,10 @@
 
     <v-row class="introduction" style="font-family: dot;">
       <v-col class="sub-introduction main" cols="12" sm="12" md="12" lg="12">
-        <h3 class="login-explain">人間は成長してこそ楽しさを感じる</h3>
-        <h3 class="login-explain">さらなる高みを目指して</h3>
+        <h3 class="login-explain">ランダムな報酬が脳を活性化する</h3>
+        <h3 class="login-explain">さらなる報酬を目指して</h3>
         <h3 class="login-explain">やるべきことをゲーム化して終わらせよう！</h3>
-        <h3 class="login-explain">毎日継続</h3>
+        <h3 class="login-explain">頑張ったあとに報酬を、、、。</h3>
       </v-col>
     </v-row>
 
